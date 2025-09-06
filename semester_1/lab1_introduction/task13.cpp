f2a18689ebea09b3826acd7760a154c1b282715e
Alexander <alexandm08@gmail.com>
2025-09-06 19:21:16 +0300

    Solved 5 tasks

---
+++ semester_1/lab1_introduction/task13.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+
+    int n;
+    std::cin >> n;
+
+    long long prev1 = 0, prev2 = 1;
+    if (n >= 1) {
+        std::cout << prev1 << ' ';
+    }
+    if (n >= 2) {
+        std::cout << prev2 << ' ';
+    }
+
+    for (int i = 2; i < n; i++) {
+        long long current = prev1 + prev2;
+        std::cout << current << ' ';
+        prev1 = prev2;
+        prev2 = current;
+    }
+
+    std::cout << '\n';
+}
