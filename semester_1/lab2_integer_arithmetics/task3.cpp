c4ed830830863b2df6a20e9a104d4135f031d6dd
Alexander <alexandm08@gmail.com>
2025-09-24 10:44:06 +0300

    Fixed task3

--- semester_1/lab2_integer_arithmetics/task3.cpp
+++ semester_1/lab2_integer_arithmetics/task3.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
+#include <cmath>
+
+bool OnlyNumbers(std::string s) {
+    for (char now : s) {
+        if (!(now >= '0' && now <= '9')) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main () {
-    std::string s;
-    if (!(std::cin >> s)) {
-        std::cout << "Error\n";
-        std::exit(1);
-    }
     
-    std::cout << '1' << s << '1' << '\n';
+    std::string num;
+    std::cout << "Enter a natural number:\n";
+
+    if (!(std::cin >> num)) {
+        std::cout << "Error\n";
+        return 1;
+    }
+
+    if (num.empty() || !OnlyNumbers(num) || (num.size() > 1 && num[0] == '0')) {
+        std::cout << "Error\n";
+        return 1;
+    }
+
+    char min_digit = '9';
+    for (char c : num) {
+        if (c != '0' && c < min_digit) {
+            min_digit = c;
+        }
+    }
+    std::string result;
+    result += min_digit;
+    result += num;
+    result += min_digit;
+    
+    std::cout << result << '\n';
 }
