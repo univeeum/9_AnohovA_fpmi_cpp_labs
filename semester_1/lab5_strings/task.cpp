3f6e4420905e1e4e8a4afc368c7aa0d587ee7e6f
Alexander <alexandm08@gmail.com>
2025-10-15 12:10:23 +0300

    Solved lab :/

--- semester_1/lab5_strings/task.cpp
+++ semester_1/lab5_strings/task.cpp
@@ -2,45 +2,8 @@
 #include <iostream>
 #include <vector>
 
-void get_longest(const std::vector<std::string> &words) {
-
-    if (words.empty()) {
-        std::cout << "There are no words in string\n";
-        return;
-    }
-    // TODO size_t поч c ним что то не работает исправить
-    int max_len = INT_MIN;
-    for (const auto &word : words) {
-        int now_len = static_cast<int>(word.length());
-        max_len = std::max(max_len, now_len);
-    }
-
-    for (const auto &word : words) {
-        int now_len = static_cast<int>(word.length());
-        if (now_len == max_len)
-            std::cout << word << ' ';
-    }
-    std::cout << '\n';
-}
-
-int main() {
-    // TODO как нормально получить символы разделители? \n и тд и что если пользователь их не введет
-    // Я не понимаю условие, дается строка или последовательность символов и что ваще АААА
-    std::cout << "Input separator characters one after another\n";
-    std::string sep_line;
-    getline(std::cin, sep_line);
-
-    // Если пользователь не ввел разделители, используем пробел по умолчанию
-    if (sep_line.empty()) {
-        sep_line = " ";
-    }
-
-    std::cout << "Input line\n";
-    std::string line;
-    getline(std::cin, line);
-
-    std::vector<std::string> words;
-
+void find_words(const std::string &sep_line, const std::string &line, std::vector<std::string> &words) {
+    
     std::string word = "";
     for (char sym : line) {
         bool is_separator = false;
@@ -50,7 +13,6 @@ int main() {
                 break;
             }
         }
-
         if (is_separator) {
             if (!word.empty())
                 words.push_back(word);
@@ -59,11 +21,53 @@ int main() {
             word += sym;
         }
     }
-
     if (!word.empty()) {
         words.push_back(word);
     }
+}
 
+void get_longest(const std::vector<std::string> &words) {
+
+    if (words.empty()) {
+        std::cout << "There are no words in string\n";
+        return;
+    }
+    
+    size_t max_len = 0;
+    for (const auto &word : words) {
+        size_t now_len = word.length();
+        max_len = std::max(max_len, now_len);
+    }
+
+    for (const auto &word : words) {
+        size_t now_len = word.length();
+        if (now_len == max_len)
+            std::cout << word << ' ';
+    }
+    std::cout << '\n';
+}
+
+int main() {
+    // Input separator string
+    std::cout << "Input separator characters one after another\n";
+    std::string sep_line;
+    getline(std::cin, sep_line);
+
+    // if no separators then use space symbol
+    if (sep_line.empty()) {
+        sep_line = " ";
+    }
+
+    // Input line 
+    std::cout << "Input line\n";
+    std::string line;
+    getline(std::cin, line);
+
+    // Find words in line
+    std::vector<std::string> words;
+    find_words(sep_line, line, words);
+
+    // Print longest words
     get_longest(words);
 
     return 0;
