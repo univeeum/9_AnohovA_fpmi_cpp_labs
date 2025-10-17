c1c655a3c6c35aea01b20d30a268d4cbdfdde4c6
Alexander <alexandm08@gmail.com>
2025-10-17 12:53:10 +0300

    Solve lab

--- semester_1/lab6_files/task.cpp
+++ semester_1/lab6_files/task.cpp
@@ -1,6 +1,66 @@
-#include <iostream> 
+#include <cctype>
+#include <fstream>
+#include <iostream>
+#include <vector>
+// TODO solve lab 6 and 7
 
-int main () {
-    // Todo послушать про stream на занятии и сделать лабу
+int count_words(const std::string &str) {
+    bool is_word = false;
+    int counter = 0;
+    int answer = 0;
+    for (size_t i = 0; i < str.length(); i++) {
+        if (!std::isspace(str[i])) {
+            if (!is_word) {
+                is_word = true;
+                answer++;
+            }
+        } else {
+            is_word = false;
+        }
+    }
+
+    return answer;
+}
+
+int main() {
+
+    std::ifstream fin("../input.txt");
+    if (!fin.is_open()) {
+        std::cout << "Error opening file :(\n";
+        std::exit(1);
+    }
+
+    int max_word_count = 0;
+    bool found_non_empty_line = false;
+
+    std::vector<std::string> lines_with_max_words;
+
+    std::string str;
+    while (getline(fin, str)) {
+        int word_count = count_words(str);
+        if (word_count > 0) {
+            found_non_empty_line = true;
+        }
+        if (word_count > max_word_count) {
+            max_word_count = word_count;
+            lines_with_max_words.clear();
+            lines_with_max_words.push_back(str);
+        } else if (word_count == max_word_count) {
+            lines_with_max_words.push_back(str);
+        }
+    }
+
+    if (!found_non_empty_line) {
+        std::cout << "All lines in the file are empty or contain no words\n";
+        fin.close();
+        return 0;
+    }
+
+    int len = lines_with_max_words.size();
+    for (int i = 0; i < std::min(10, len); i++) {
+        std::cout << lines_with_max_words[i] << '\n';
+    }
+
+    fin.close();
     return 0;
 }
